194b738b2a99e7d30d4abaef5852fb2c0065072e
Laurent Magnin <laugimethods@Huard.local>
2016-03-30 08:59:23 -0400

    Back to provided code
    https://hub.docker.com/r/dylanmei/zeppelin/

--- install.sh
+++ install.sh
@@ -8,8 +8,11 @@ DOCKERFILE
 cat > ./install.sh <<INSTALL
 git pull
 mvn clean package -DskipTests \
-  -Pspark-1.6 \
-  -Dspark.version=1.6.1 \
+  -Pspark-1.5 \
+  -Dspark.version=1.5.2 \
+  -Phadoop-2.2 \
+  -Dhadoop.version=2.0.0-cdh4.2.0 \
+  -Pyarn
 INSTALL
 
 docker build -t my_zeppelin .
